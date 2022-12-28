b0080db1bffc5c99379d2c7cf99c070476659ef5
Pablo203 <pawelrosa686@gmail.com>
2022-12-28 14:48:51 -0500

    Add first transform

--- swexplorer/urls.py
+++ swexplorer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CollectionsListView, fetchCollection
+from .views import CollectionsListView, fetchCollection, CollectionListView
 
 urlpatterns = [
     path('', CollectionsListView.as_view(), name='collectionsList'),
-    path('fetchCollection/', fetchCollection, name="fetchCollection")
+    path('fetchCollection/', fetchCollection, name="fetchCollection"),
+    path('<str:fileName>/10/', CollectionListView.as_view(), name='collectionList'),
 ]
--- swexplorer/views.py
+++ swexplorer/views.py
@@ -2,31 +2,78 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.utils.crypto import get_random_string
-import string
+from string import ascii_uppercase
 from django.views.generic import ListView
 from .models import Collection
 import requests
 import csv
+import petl as etl
+import logging
+from datetime import datetime
+import os
+_logger = logging.getLogger('django')
 
 # Create your views here.
 class CollectionsListView(ListView):
     model = Collection
 
-    queryset = Collection.objects.all()[:10]
+    context_object_name = 'collections'
+
+    queryset = Collection.objects.all().order_by('-date')[:10]
 
     template_name = "collections_list.html"
 
+class CollectionListView(ListView):
+    model = Collection
 
+    context_object_name = 'collection'
+
+    template_name = 'collection_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['fileName'] = self.kwargs['fileName']
+        return context
 
 def fetchData():
   r = requests.get('https://swapi.dev/api/people')
   # Export the data for use in future steps
   return r.json()
 
+def checkForChar(stringToCheck):
+    for char in stringToCheck:
+        if char in ascii_uppercase:
+            stringToCheck = stringToCheck.replace(char, ' ')
+    return stringToCheck
+
+def changeDateFormat(dateField):
+    _logger.info(datetime.strptime(dateField, '%y-%m-%d %H:%M:%S'))
+
+def cleanCsv(filePath, fileName):
+    table1 = etl.fromcsv(filePath)
+    table2 = etl.cutout(table1, 'films')
+    table3 = etl.cutout(table2, 'species')
+    table4 = etl.cutout(table3, 'vehicles')
+    table5 = etl.cutout(table4, 'starships')
+    table6 = etl.cutout(table5, 'created')
+    table7 = etl.cutout(table6, 'url')
+    #table8 = etl.addfield(table7, 'date', lambda row: row['edited'])
+    #table9 = etl.convert(table8, 'date', lambda row: checkForChar(row))
+    
+    table10 = etl.convert(table7, 'homeworld', lambda row: requests.get(row).json()['name'])
+    #table11 = etl.convert(table10, 'date', lambda row: changeDateFormat(row))
+    #table12 = etl.convert(table11, 'date', lambda row: row.datetime.strftime('%y-%m-%d'))
+    etl.tocsv(table10, ("csvFiles/a" + fileName))
+    if os.path.exists(filePath):
+        os.remove(filePath)
+
 def fetchCollection(request):
+    #Get data from API
     jsonData = fetchData()['results']
+    #Generate unique name for file and create file in system
     fileName = get_random_string(20) + ".csv"
     dataFile = open('csvFiles/%s' % fileName, 'w')
+    #Prepare new record in database
     collection = Collection(
         filePath = 'csvFiles/%s' % fileName,
         fileName = fileName
@@ -34,14 +81,19 @@ def fetchCollection(request):
     csv_writer = csv.writer(dataFile)
     count = 0
 
+    #Convert data from JSON to CSV
     for data in jsonData:
         if count == 0:
             header = data.keys()
             csv_writer.writerow(header)
             count += 1
+    #Save file and create new record
     csv_writer.writerow(data.values())
  
     dataFile.close()
     collection.save()
+
+    #Transform csv file
+    cleanCsv('csvFiles/%s' % fileName, fileName)
 
     return redirect('collectionsList')
