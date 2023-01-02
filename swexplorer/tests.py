a9a575efc49b58807d3f925b922e4ec69502f1ff
Pablo203 <pawelrosa686@gmail.com>
2023-01-02 14:06:04 -0500

    Add tests and comment

--- swexplorer/tests.py
+++ swexplorer/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
+from .views import *
+from datetime import datetime
+import json
+from django.utils.crypto import get_random_string
+import csv
 
 # Create your tests here.
+class UrlsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def setUpCsv(self):
+        jsonData = fetchData()['results']
+        self.fileName = get_random_string(20) + ".csv"
+        self.file = open('csvFiles/a%s' % self.fileName, 'w')
+        count = 0
+        csv_writer = csv.writer(self.file)
+        for data in jsonData:
+            if count == 0:
+                header = data.keys()
+                csv_writer.writerow(header)
+                count += 1
+    #Save file and create new record
+            csv_writer.writerow(data.values())
+        self.file.close()
+        self.filePath = 'csvFiles/a%s' % self.fileName
+
+    def cleanUp(self):
+        if os.path.exists(self.filePath):
+            os.remove(self.filePath)
+
+    def test_data_fetch_data(self):
+        response = fetchData()
+        try:
+            json.loads(response)
+            return True
+        except:
+            return False
+
+    def test_changing_date_format(self):
+        dateTime = datetime.now()    
+        try:
+            date = changeDateFormat(datetime.strftime(dateTime, '%Y-%m-%d%H:%M:%S.%f'))
+            datetime.strptime(date, '%Y-%m-%d')
+            return True
+        except:
+            return False
+
+
+    def test_collection_list_view(self):
+        response = self.client.get(reverse('collectionsList'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'collections_list.html')
+
+    def test_fetch_collection_show_10(self):
+        self.setUpCsv()
+        response = self.client.get(reverse('collectionList', kwargs={'fileName': self.fileName}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'collection_list.html')
+        self.cleanUp()
+
+    def test_fetch_collection_show_20(self):
+        self.setUpCsv()
+        response = self.client.get(reverse('collectionList20', kwargs={'fileName': self.fileName}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'collection_list.html')
+        self.cleanUp()
+    
+    def test_collection_count_view(self):
+        self.setUpCsv()
+        response = self.client.get(reverse('collectionCount', kwargs={'fileName': self.fileName, 'columns': 'height-name-'}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'collection_count_list.html')
+        self.cleanUp()
--- swexplorer/views.py
+++ swexplorer/views.py
@@ -19,7 +19,8 @@ class CollectionsListView(ListView):
 
     context_object_name = 'collections'
 
-    queryset = Collection.objects.all().order_by('-date')[:10]
+    #Get all datasets and order by date - show latest dataset on top
+    queryset = Collection.objects.all().order_by('-date')
 
     template_name = "collections_list.html"
 
@@ -30,10 +31,12 @@ class CollectionListView(ListView):
 
     template_name = 'collection_list.html'
 
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['fileName'] = self.kwargs['fileName']
         context['csvFileContent'] = etl.fromcsv('csvFiles/a' + self.kwargs['fileName'])
+        #Limit displayed data to first 10 rows
         context['csvFileContent'] = etl.rowslice(context['csvFileContent'], 10)
         return context
 
@@ -43,6 +46,7 @@ class CollectionListViewShowMore(CollectionListView, ListView):
         context = super().get_context_data(**kwargs)
         context['fileName'] = self.kwargs['fileName']
         context['csvFileContent'] = etl.fromcsv('csvFiles/a' + self.kwargs['fileName'])
+        #Extend displayed data to 20 rows
         context['csvFileContent'] = etl.rowslice(context['csvFileContent'], 20)
         return context
 
@@ -59,14 +63,20 @@ class CollectionCountListView(ListView):
         context['columns'] = self.kwargs['columns']
         context['fileName'] = self.kwargs['fileName']
         context['csvFile'] = etl.fromcsv('csvFiles/a' + self.kwargs['fileName'])
+        #Get headers for display
         context['csvHeaders'] = etl.rowslice(context['csvFile'], 0)
 
         if self.kwargs['columns'] != '0':
+            #Clear string with column names and turn it into list
             columns = self.kwargs['columns'].split("-")
+            #Remove last element which is whitespace
             columns.pop()
 
+            #Cut all columns except chosen to show
             table1 = etl.cut(context['csvFile'], columns)
+            #Get headers for display
             context['csvContent'] = etl.rowslice(table1, 0)
+            #Get names of columns
             headers = table1.header()
 
             #Prepare field to count combinations, using joined fields values
@@ -92,20 +102,30 @@ def fetchData():
 
 def changeDateFormat(dateStr):
     for char in dateStr:
+        #Check string for uppercase letters that pollutes date string
         if char in ascii_uppercase:
             dateStr = dateStr.replace(char, '')
+    #Cast string to datetime object
     dateStr = datetime.strptime(dateStr, '%Y-%m-%d%H:%M:%S.%f')
+    #Change datetime object to date
     dateStr = dateStr.date()
     return dateStr
 
 def cleanCsv(filePath, fileName):
+    #Open csv file
     table1 = etl.fromcsv(filePath)
+    #Remove unneccessary columns
     table2 = etl.cutout(table1, 'films', 'species', 'vehicles', 'starships', 'created', 'url')
+    #Add new field based from another column
     table3 = etl.addfield(table2, 'date', lambda row: row['edited'])
+    #Modify column to show wanted date format
     table4 = etl.convert(table3, 'date', lambda row: changeDateFormat(row))
+    #Remove used column
     table5 = etl.cutout(table4, 'edited')
+    #Resolve all planets for limiting requests numbers
     table6 = etl.convert(table5, 'homeworld', lambda row: requests.get(row).json()['name'])
     
+    #To evade error remove old csv file and save new one
     etl.tocsv(table6, ("csvFiles/a" + fileName))
     if os.path.exists(filePath):
         os.remove(filePath)
@@ -113,6 +133,7 @@ def cleanCsv(filePath, fileName):
 def fetchCollection(request):
     #Get data from API
     jsonData = fetchData()['results']
+    
     #Generate unique name for file and create file in system
     fileName = get_random_string(20) + ".csv"
     dataFile = open('csvFiles/%s' % fileName, 'w')
@@ -130,9 +151,10 @@ def fetchCollection(request):
             header = data.keys()
             csv_writer.writerow(header)
             count += 1
-    #Save file and create new record
-    csv_writer.writerow(data.values())
- 
+        #Save file and create new record
+        csv_writer.writerow(data.values())
+    
+    #Save file and created record
     dataFile.close()
     collection.save()
 
